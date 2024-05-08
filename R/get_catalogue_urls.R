f1413c1f116adf3dbb488034a86a9b7a2bd3e79e
Howard Askew <19271569+howaskew@users.noreply.github.com>
2024-05-08 15:41:43 +0100

    Added tests

--- R/get_catalogue_urls.R
+++ R/get_catalogue_urls.R
@@ -1,18 +1,25 @@
+#' Get OpenActive data catalogue URLs
+#'
+#' @return A list of OpenActive catalogue URLs
+#' @export
+#'
+#' @examples
+#' catalogue_urls <- get_catalogue_urls()
 get_catalogue_urls <- function() {
+  catalogue_urls <- list()
   collection_url <- 'https://openactive.io/data-catalogs/data-catalog-collection.jsonld'
-  res = try(GET(collection_url), silent = T)
+  res = try(httr::GET(collection_url), silent = T)
   if (!inherits(res, "try-error")) {
     if (res$status_code >= 200 && res$status_code < 400) {
       #If status ok, read content
       data=rawToChar(res$content)
-      if (validate(data)==T) {
-        data=fromJSON(rawToChar(res$content))$hasPart
+      if (jsonlite::validate(data)==T) {
+        data=jsonlite::fromJSON(rawToChar(res$content))$hasPart
       }
       if (length(data)>0) {
         catalogue_urls <- as.list(data)
-      } else
-        catalogue_urls <- list()
+      }
     }
   }
+  catalogue_urls
 }
-
---
+++ tests/testthat/test-get_catalogue_urls.R
@@ -0,0 +1,3 @@
+test_that("get_catalogue_urls() returns a list", {
+  expect_true(is.list(get_catalogue_urls()))
+})
